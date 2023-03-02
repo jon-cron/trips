b330bb4c219298e77c0428226e82d5cb67aa3f15
jon-cron <jonathanacm95@gmail.com>
2023-03-02 12:52:58 -0700

    error handling

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,14 +3,23 @@ import React, { useState, useEffect } from "react";
 export const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       setIsPending(true);
-      const response = await fetch(url);
-      const json = await response.json();
-      setIsPending(false);
-      setData(json);
+
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setIsPending(false);
+        setData(json);
+        setError(null);
+      } catch (error) {
+        setIsPending(false);
+        setError("Bad Request");
+        console.log(error.message);
+      }
     };
     fetchData();
   }, [url]);
