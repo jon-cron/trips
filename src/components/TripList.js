7e42870d981d89e1d1c36f05eec2a69288ba7cf4
jon-cron <jonathanacm95@gmail.com>
2023-03-02 12:01:48 -0700

    understanding useEffect

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -2,16 +2,27 @@ import React, { useState, useEffect } from "react";
 
 export default function TripList() {
   const [trips, setTrips] = useState([]);
+  const [url, setUrl] = useState("http://localhost:3000/trips");
   useEffect(() => {
-    fetch("http://localhost:3000/trips")
+    // NOTE whenever there is variable that is dynamic such a url and can change, place the variable in the dependency array
+    fetch(url)
       .then((response) => response.json())
       .then((json) => setTrips(json));
-  }, []);
+    // NOTE here is the dependency array
+  }, [url]);
   console.log(trips);
 
   return (
     <div>
       <h2>TripList</h2>
+      <ul>
+        {trips.map((trip) => (
+          <li key={trip.id}>
+            <h2>{trip.title}</h2>
+            <p>${trip.price}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
